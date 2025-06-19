847c8e319714730d46ee16245d5bf4864d4cf1bc
vinilcinhu <vinicius_vck@hotmail.com>
2025-06-19 16:15:46 -0300

    Implemetacao do Bispo

--- projeto_xadrez/pecas/Bispo.java
+++ projeto_xadrez/pecas/Bispo.java
@@ -11,8 +11,33 @@ public class Bispo extends Peca {
     }
 
     @Override
-    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) { return true; } // implementar
+    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) {
+        int diffLinha = Math.abs(linhaO - linhaD);
+        int diffColuna = Math.abs(colunaO - colunaD);
+        return diffLinha == diffColuna;
+    }
 
     @Override
-    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD) { return ""; } // implementar
+    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD) {
+        if (!movimentoValido(linhaO, colunaO, linhaD, colunaD)) {
+            return "";
+        }
+
+        StringBuilder caminho = new StringBuilder();
+
+        int dirLinha = linhaD > linhaO ? 1 : -1;
+        int dirColuna = colunaD > colunaO ? 1 : -1;
+
+        int linha = linhaO + dirLinha;
+        char coluna = (char) (colunaO + dirColuna);
+
+        while (linha != linhaD && coluna != colunaD) {
+            caminho.append(linha).append(coluna);
+            linha += dirLinha;
+            coluna += dirColuna;
+        }
+
+        caminho.append(linhaD).append(colunaD); // Inclui destino
+        return caminho.toString();
+    }
 }
