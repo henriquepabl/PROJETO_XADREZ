d1833cec1b84f61e206b6e7bdd45c5b488817331
vinilcinhu <vinicius_vck@hotmail.com>
2025-06-19 13:08:20 -0300

    Implementacao da Torre

--- projeto_xadrez/pecas/Torre.java
+++ projeto_xadrez/pecas/Torre.java
@@ -11,8 +11,32 @@ public class Torre extends Peca {
     }
 
     @Override
-    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) { return true; } // implementar
+    public boolean movimentoValido(int linhaO, char colunaO, int linhaD, char colunaD) {
+        // Movimento na mesma linha ou na mesma coluna
+        return (linhaO == linhaD) || (colunaO == colunaD);
+    }
 
     @Override
-    public String caminho(int linhaO, char colunaO, int linhaD, char colunaD) { return ""; } // implementar
+   public String caminho(int linhaO, char colunaO, int linhaD, char colunaD) {
+        if (!movimentoValido(linhaO, colunaO, linhaD, colunaD)) {
+            return "";
+        }
+
+        StringBuilder caminho = new StringBuilder();
+
+        if (linhaO == linhaD) { // Movimento horizontal
+            int dir = colunaD > colunaO ? 1 : -1;
+            for (char c = (char) (colunaO + dir); c != colunaD; c += dir) {
+                caminho.append(linhaO).append(c);
+            }
+        } else if (colunaO == colunaD) { // Movimento vertical
+            int dir = linhaD > linhaO ? 1 : -1;
+            for (int l = linhaO + dir; l != linhaD; l += dir) {
+                caminho.append(l).append(colunaO);
+            }
+        }
+
+        caminho.append(linhaD).append(colunaD); // Inclui destino
+        return caminho.toString();
+    }
 }
